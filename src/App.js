d1632e64b5f786cfda85f5cdb9defce5918c4fa1
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-06-04 14:47:19 +0300

    exc-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import WarningSign from "./components/WarningSign";
 
 function App() {
   return (
     <div className="App">
       <NavBar title="StriveBooks" />
       <Home jumboTitle="Welcome to strivebooks" />
+      <WarningSign/>
     </div>
   );
 }
---
+++ src/components/WarningSign.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Alert} from 'react-bootstrap';
+
+
+const WarningSign = () => {
+    return (
+        <Alert  variant="primary">
+    This is a alert—check it out!
+        </Alert>
+    )
+}
+
+
+export default WarningSign;
